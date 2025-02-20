a7d8bf0bb2861e3c07894ea232639b148e66d689
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-20 20:56:59 -0300

    Aula de coleções

---
+++ Java/Colecoes/src/br/com/fiap/teste/TesteMap.java
@@ -0,0 +1,12 @@
+package br.com.fiap.teste;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TesteMap {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Teste 1");
+        map.put(2, "Teste 2");
+    }
+}
---
+++ Java/Colecoes/src/br/com/fiap/teste/TesteSet.java
@@ -0,0 +1,12 @@
+package br.com.fiap.teste;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TesteSet {
+    public static void main(String[] args) {
+        Set<String> treeSet = new TreeSet<>();
+        treeSet.add("Elemento 1");
+        treeSet.add("ELemento 2");   
+    }
+}

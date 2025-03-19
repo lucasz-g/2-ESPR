9379af0e4870d77b165e088335ba840fe122a603
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-18 21:44:41 -0300

    Reestruturando

--- Java/IntegracaoBD/src/integracaoBD/Teste.java
+++ Java/IntegracaoBD/src/integracaoBD/Teste.java
@@ -2,30 +2,55 @@ package integracaoBD;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Teste {
-    public static void main(String[] args) {
-        String url = "jdbc:oracle:thin:@localhost:1521:XE"; // URL para Oracle
-        String user = "admin_lucas"; // Insira o seu usuário
-        String password = "root"; // Insira a sua senha
-        
-        String insert = "INSERT INTO ALUNO (ID, NOME, DOCUMENTO) VALUES (?, ?, ?)";
-        
-        try (Connection connection = DriverManager.getConnection(url, user, password);
-            Statement statement = connection.createStatement()) {
-        	
-        	String createTable = "CREATE TABLE ALUNO ("
-        	        + "ID NUMBER, "
-        	        + "NOME VARCHAR(100) NOT NULL, "
-        	        + "DOCUMENTO VARCHAR(100) NOT NULL, "
-        	        + "PRIMARY KEY (ID)"
-        	        + ")";      	
-        	statement.executeUpdate(createTable);
-        	System.out.println("Tabela criada com sucesso!");
-        } catch (SQLException e ) {
-        	e.printStackTrace();
-        }
-    }       
+
+	public static void main(String[] args) throws SQLException {
+		
+		String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
+		String user = "rm554070";
+		String password = "fiap25";
+		
+		String insert = "INSERT INTO ALUNO (ID, NOME, DOCUMENTO) VALUES (?, ?, ?)";
+		
+		String select = "SELECT * FROM ALUNO";
+		
+		Connection conn = null;
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			preparedStatement = conn.prepareStatement(insert);
+			preparedStatement.setInt(1, 2);
+			preparedStatement.setString(2, "Teste");
+			preparedStatement.setString(3, "887979");
+			preparedStatement.executeUpdate();
+			System.out.println("Inclusão efetuada com sucesso");
+			
+			preparedStatement.close();
+			
+			preparedStatement = conn.prepareStatement(select);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			
+			while (resultSet.next()) {
+				int id = resultSet.getInt("ID");
+				String nome = resultSet.getString("NOME");
+				String documento = resultSet.getString("DOCUMENTO");
+				
+				System.out.println("ID = " + id + " NOME = " + nome + " DOCUMENTO = " + documento);
+			}
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			preparedStatement.close();
+			conn.close();
+		}
+
+	}
+
 }
